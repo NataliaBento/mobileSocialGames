d39f003f587e72b1a8a4b87a51aac1d61c8b3989
Natalia Bento <nataliabento97@gmail.com>
2025-04-28 11:19:48 -0300

    feat: cria o model de jogo

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 
 import authRoutes from "./routes/authRoutes.js";
+import gamesRoutes from "./routes/gamesRoutes.js";
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -10,6 +12,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/api/auth", authRoutes); 
+app.use("/api/games", gamesRoutes); 
+
+
+
 app.listen(PORT, () => {
     console.log(`Servidor Rodando na porta ${PORT}`);
     connectDB();
